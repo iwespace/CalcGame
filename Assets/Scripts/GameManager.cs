d0e6eeeaf901b57eea430c1d19eb9cbc0ee39961
iwespace <iwespace@gmail.com>
2018-11-22 16:01:44 +0900

    버튼 위치 랜덤 생성

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+    public Button Ans = null;
+    public Button AnsPlusBtn = null;
+    public Button AnsMinusBtn = null;
+
+
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        int pos = Random.Range(-600, 600);
+        Vector3 Vpos = new Vector3(pos, pos);
+        Ans.transform.position = Vpos;
+        AnsPlusBtn.transform.position = Vpos;
+        AnsMinusBtn.transform.position = Vpos;
+    }
 	
 	// Update is called once per frame
 	void Update () {
