05a3faba719a61bca65fa13318d4fbe1eafa8133
majesticgow <majesticgow@naver.com>
2018-11-22 15:21:57 +0900

    Add Random Text UI

---
+++ Assets/Scripts/RandomText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomText : MonoBehaviour {
+
+    [SerializeField]
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+
+        var randomNumber = Random.Range(0, 10);
+        randomNumber += 1;
+        text.text = "Random : " + randomNumber.ToString();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
